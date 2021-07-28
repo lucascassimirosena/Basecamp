1ac4e1c9a30b5c4760d44f4b5959f73088375682
lcassimi <lcassimi@student.42sp.org.br>
2021-07-28 16:43:24 +0000

    I worked on the C01 activities

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,7 @@
+void	ft_ultimate_ft(int *********nbr)
+{
+	int	l;
+
+	l = 42;
+	*********nbr = l;
+}
---
+++ C01/ex02/ft_swap.c
@@ -0,0 +1,8 @@
+void	ft_swap(int *a, int *b)
+{
+	int	basecamp;
+
+	basecamp = *a;
+	*a = *b;
+	*b = basecamp;
+}
